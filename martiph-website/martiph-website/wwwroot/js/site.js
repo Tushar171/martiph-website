9d46374ba1af412c6e4b3b322b4c2533d7c6ebe1
martiph <philip_marti@besonet.ch>
2020-09-23 11:40:00 +0200

    added css animations

--- martiph-website/martiph-website/wwwroot/js/site.js
+++ martiph-website/martiph-website/wwwroot/js/site.js
@@ -2,16 +2,4 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your Javascript code.
-const sleep = ms => new Promise(r => setTimeout(r, ms));
 
-/*addEventListener('load', async ()=>{
-    let skull = document.querySelector('.skull');
-    while(true){
-
-    
-    skull.style.visibility = 'hidden';
-    await sleep(1000);
-    skull.style.visibility = 'visible';
-    await sleep(1000);    
-}
-})*/
